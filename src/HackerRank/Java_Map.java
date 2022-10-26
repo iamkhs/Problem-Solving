86cfbc3c6276b4d31d592e114bb9e232b5c2b6ed
Kibriar Hossain <shantohossain.p@gmail.com>
2022-10-27 02:26:49 +0600

    HackerRank Java_Map solution

---
+++ src/HackerRank/Java_Map.java
@@ -0,0 +1,36 @@
+package HackerRank;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Java_Map {
+    public static void main(String []args){
+
+        Map<String, Integer> phoneBook = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        //System.out.print("Enter n number : ");
+        int n = input.nextInt();
+        input.nextLine();
+
+        for (int i = 0; i<n; i++){
+            //System.out.print("Enter name : ");
+            String name = input.nextLine().toLowerCase();
+
+            //System.out.print("Enter phone : ");
+            int phone = input.nextInt();
+            phoneBook.put(name,phone);
+            input.nextLine();
+        }
+
+        while(input.hasNext()){
+            String s = input.nextLine();
+            if (phoneBook.get(s) != null){
+                System.out.println(s+"="+phoneBook.get(s));
+            }
+            else {
+                System.out.println("Not found");
+            }
+        }
+    }
+}
