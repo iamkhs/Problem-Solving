918a5248a9ae297c1c9f16581d5af72188243631
Kibriar Hossain <shantohossain.p@gmail.com>
2022-11-30 02:59:21 +0600

    LeetCode Majority Element solution

---
+++ src/leet_code/MajorityElement.java
@@ -0,0 +1,15 @@
+package leet_code;
+// https://leetcode.com/problems/majority-element/
+import java.util.Arrays;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
