f8c1f08f83ffa1b3b1eed93393c94523c3791155
Kibriar Hossain <shantohossain.p@gmail.com>
2022-12-10 22:19:00 +0600

    Find Duplicate Number in an Array  solution with Cyclic sort Algorithm

--- src/leet_code/FindDuplicateNumber.java
+++ src/leet_code/FindDuplicateNumber.java
@@ -8,11 +8,11 @@ public class FindDuplicateNumber {
     public static void main(String[] args){
 
         int [] nums = {1,3,4,2,2};
-        System.out.println(findDuplicate(nums));
+        System.out.println(findDuplicateWithCyclicSort(nums));
     }
 
     // Using HashSet time complexity is BigO(n)
-    public static int findDuplicate_set(int[] nums) {
+    private static int findDuplicate_set(int[] nums) {
         Set<Integer> set = new HashSet<>();
         int size = nums.length;
         for (int i = 0; i < size; i++) {
@@ -24,11 +24,32 @@ public class FindDuplicateNumber {
     }
 
     // Linear search Algorithms time complexity is BigO(n^2)
-    public static int findDuplicate(int[] nums) {
+    private static int findDuplicate(int[] nums) {
         Arrays.sort(nums);
         for (int i = 0; i< nums.length; i++){
             if (nums[i] == nums[i+1]){
                 return nums[i];
+            }
+        }
+        return -1;
+    }
+
+    // Cyclic Sort algorithm with BigO(n) time complexity more optimized
+    private static int findDuplicateWithCyclicSort(int[] nums) {
+        int i = 0;
+        while(i < nums.length){
+            int current = nums[i] - 1;
+            if (nums[i] != nums[current]){
+                int temp = nums[i];
+                nums[i] = nums[current];
+                nums[current] = temp;
+            }
+            else i++;
+        }
+
+        for (int index = 0; index< nums.length; index++){
+            if (index+1 != nums[index]){
+                return nums[index];
             }
         }
         return -1;
