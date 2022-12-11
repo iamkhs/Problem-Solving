49a8c003c1495be048955599fb5c49c1a9f4869c
Kibriar Hossain <shantohossain.p@gmail.com>
2022-12-11 22:56:10 +0600

    Missing Number solution with Cyclic sort Algorithm

--- src/leet_code/MissingNumber.java
+++ src/leet_code/MissingNumber.java
@@ -22,4 +22,25 @@ public class MissingNumber {
         return 0;
     }
 
+    // Another Approach using Cyclic Sort Algorithm with 1ms most efficient solution
+    private static int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length){
+            int current = nums[i]-1;
+            if (nums[i]>0 && nums[i] < nums.length && nums[i] != nums[current]){
+                int temp = nums[i];
+                nums[i] = nums[current];
+                nums[current] = temp;
+            }
+            else i++;
+        }
+
+        for (int index = 0; index< nums.length; index++){
+            if (index+1 != nums[index]){
+                return index+1;
+            }
+        }
+        return nums.length+1;
+    }
+
 }
