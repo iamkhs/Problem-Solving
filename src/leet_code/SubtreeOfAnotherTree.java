8a701c0c2cbc525098698ee9d1a59e6427a5e320
Kibriar Hossain <shantohossain.p@gmail.com>
2023-02-24 11:29:05 +0600

    LeetCode Subtree Of Another Tree Solution

---
+++ src/leet_code/SubtreeOfAnotherTree.java
@@ -0,0 +1,23 @@
+package leet_code;
+// https://leetcode.com/problems/subtree-of-another-tree/description/
+public class SubtreeOfAnotherTree {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null && subRoot == null) return true;
+        if(root == null || subRoot == null) return false;
+
+        if(root.val == subRoot.val){
+            if(isSame(root, subRoot))return true;
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean isSame(TreeNode root, TreeNode subRoot){
+        if(root == null && subRoot == null) return true;
+        if(root == null || subRoot == null) return false;
+
+        if(root.val != subRoot.val) return false;
+
+        return isSame(root.left, subRoot.left) && isSame(root.right, subRoot.right);
+    }
+}
