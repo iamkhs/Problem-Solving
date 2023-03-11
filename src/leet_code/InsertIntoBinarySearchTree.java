93b6d27aff9c1cb9c4935c317bf2d8e51393306d
Kibriar Hossain <shantohossain.p@gmail.com>
2023-03-11 22:04:09 +0600

    LeetCode Insert into Binary Search Tree Solution

---
+++ src/leet_code/InsertIntoBinarySearchTree.java
@@ -0,0 +1,45 @@
+package leet_code;
+// https://leetcode.com/problems/insert-into-a-binary-search-tree/description/
+public class InsertIntoBinarySearchTree {
+
+    // Iterative Solution with O(1) space complexity
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        TreeNode newNode = new TreeNode(val);
+        if(root == null) return newNode;
+
+        TreeNode curr = root;
+        TreeNode parent = null;
+
+        while(curr != null){
+            parent = curr;
+            if(val > curr.val){
+                curr = curr.right;
+            }
+            else curr = curr.left;
+        }
+
+        if(val > parent.val){
+            parent.right = newNode;
+        }
+        else parent.left = newNode;
+
+        return root;
+    }
+
+    // Recursive Solution with O(h) space complexity, h stand for height of the tree
+    public TreeNode recursiveInsertIntoBST(TreeNode root, int val){
+        if(root == null){
+            root = new TreeNode(val);
+        }
+
+        if(val > root.val) {
+            root.right = recursiveInsertIntoBST(root.right, val);
+        }
+
+        else if(val < root.val) {
+            root.left = recursiveInsertIntoBST(root.left, val);
+        }
+
+        return root;
+    }
+}
