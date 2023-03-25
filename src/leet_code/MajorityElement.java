6cdcbafdf07f3da492c80e2f0bd5607d8796eec9
Kibriar Hossain <shantohossain.p@gmail.com>
2023-03-25 17:47:16 +0600

    LeetCode Majority Element Solution

--- src/leet_code/MajorityElement.java
+++ src/leet_code/MajorityElement.java
@@ -1,15 +1,35 @@
 package leet_code;
 // https://leetcode.com/problems/majority-element/
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MajorityElement {
     public static void main(String[] args) {
         int[] nums = {2, 2, 1, 1, 1, 2, 2};
-        System.out.println(majorityElement(nums));
+        System.out.println(majorityElement2(nums));
     }
 
     public static int majorityElement(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length / 2];
+    }
+
+    // Using HashMap
+    public static int majorityElement2(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else map.put(num, 1);
+        }
+
+        int n = nums.length;
+        for (int i :  map.keySet()){
+            if (map.get(i) > n / 2){
+                return i;
+            }
+        }
+        return -1;
     }
 }
